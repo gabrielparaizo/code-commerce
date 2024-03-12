59cf286bf4eeb83f983f19955c040f71877d944d
Gabriel Paraizo <36803487+gabrielparaizo@users.noreply.github.com>
2024-03-12 01:25:29 -0300

    feat(entity): add new product specification Adds specification for a new product being created

--- products-catalog/internal/entity/entity.go
+++ products-catalog/internal/entity/entity.go
@@ -22,3 +22,14 @@ type Product struct {
 	CategoryID  string
 	ImageURL    string
 }
+
+func NewProduct(name, description string, price float64, categoryID, imageURL string) *Product {
+	return &Product{
+		ID:          uuid.New().String(),
+		Name:        name,
+		Description: description,
+		Price:       price,
+		CategoryID:  categoryID,
+		ImageURL:    imageURL,
+	}
+}
